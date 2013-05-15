43fcdbd72b8acfaeadbca84927638c919c5b6f3d
Justin Hennessy <justhennessy@gmail.com>
2013-05-15 19:44:54 +1000

    More factory work.

--- spec/factories/challenge.rb
+++ spec/factories/challenge.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :challenge do
     sequence(:name) { |n| "Kilometer Smash! (past) #{n}" }
     description  "To make the most kilometers in 31 days"
-    start_date   { Time.now - 10.days }
-    end_date     { Time.now + 21.days }
+    start_date { Time.now - 10.days }
+    end_date { Time.now + 21.days }
   end
 end
