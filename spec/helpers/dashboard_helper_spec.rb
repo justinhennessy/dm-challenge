717abb1dfd2686bf07949d86795d4403782214b0
Justin Hennessy <justhennessy@gmail.com>
2013-05-20 21:51:03 +1000

    Added spec for challenge_countdown helper

--- spec/helpers/dashboard_helper_spec.rb
+++ spec/helpers/dashboard_helper_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe DashboardHelper do
+  it "display the number of days left if in the middle of a challenge" do
+    challenge = OpenStruct.new start_date: Time.now - 10.days, end_date: Time.now + 21.days
+
+    assign(:challenge, challenge)
+    helper.challenge_countdown.should include("22 days left!")
+  end
+
+  it "display the number of days until a chanllenge starts" do
+    challenge = OpenStruct.new start_date: Time.now + 10.days, end_date: Time.now + 21.days
+
+    assign(:challenge, challenge)
+    helper.challenge_countdown.should include("11 days to start!")
+  end
 end
