ba413998736347a204351add2b3ca22431678647
Justin Hennessy <justhennessy@gmail.com>
2013-05-20 20:01:07 +1000

    Layout changes

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -3,14 +3,14 @@ module DashboardHelper
     if Time.now < @challenge.start_date
       content_tag("strong", distance_of_time_in_words(Time.now, @challenge.start_date) + " to start!")
     else
-      content_tag("strong", distance_of_time_in_words(@challenge.start_date, Time.now) + " left!")
+      content_tag("strong", distance_of_time_in_words(@challenge.end_date, Time.now) + " left!")
     end
   end
 
   def style_deficit
     if @challenge.deficit == 0
       "btn"
-    elsif @challenge.deficit < 500
+    elsif @challenge.deficit < 100
       "btn btn-warning"
     else
       "btn btn-danger"
