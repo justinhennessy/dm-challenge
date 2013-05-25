d3fbcf91b8dc35303d87b62b716f62b934b8bf85
Justin Hennessy <justhennessy@gmail.com>
2013-05-25 21:43:36 +1000

    Refactored all tests to use Factory methods

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -3,30 +3,26 @@ require 'spec_helper'
 describe "Viewing the dashboard" do
 
   it "shows a list of team members and their stats" do
-    challenge = FactoryGirl.create :challenge, start_date: Time.now - 10.days, end_date: Time.now + 21.days
-
-    user1 = FactoryGirl.create :user, challenge: challenge, commitment: 1000
-    user2 = FactoryGirl.create :user, challenge: challenge, commitment: 500
-
-    FactoryGirl.create :activity, user: user1, value: 100, date: Time.now - 2.days
-    FactoryGirl.create :activity, user: user1, value: 5, date: Time.now - 1.days
-    FactoryGirl.create :activity, user: user2, value: 50, date: Time.now - 2.days
-    FactoryGirl.create :activity, user: user2, value: 200, date: Time.now - 1.days
+    challenge = create_challenge start_date: 10.days.ago, end_date: 21.days.from_now
+    user1     = create_user challenge: challenge, commitment: 1000
+    user2     = create_user challenge: challenge, commitment: 500
+    create_activity user: user1, value: 100, date: 2.days.ago
+    create_activity user: user1, value: 5, date: 1.day.ago
+    create_activity user: user2, value: 50, date: 2.days.ago
+    create_activity user: user2, value: 200, date: 1.day.ago
 
     visit dashboard_path
 
     expect(page).to have_text(user1.preferred_name)
     expect(page).to have_text(user2.preferred_name)
-
     expect(page).to have_text(user1.activity_total)
     expect(page).to have_text(user2.activity_total)
-
     expect(page).to have_text(user1.percent_completed)
     expect(page).to have_text(user2.percent_completed)
   end
 
   it "shows the number of days until the challenge starts" do
-    future_challenge = FactoryGirl.create :challenge, start_date: Time.now + 12.days, end_date: Time.now + 30.days
+    future_challenge = create_challenge start_date: 12.days.from_now, end_date: 30.days.from_now
 
     visit dashboard_path
 
@@ -34,7 +30,7 @@ describe "Viewing the dashboard" do
   end
 
   it "shows the number of days left in a challenge once it has started" do
-    challenge = FactoryGirl.create :challenge
+    challenge = create_challenge start_date: 10.days.ago, end_date: 21.days.from_now
 
     visit dashboard_path
 
@@ -42,10 +38,9 @@ describe "Viewing the dashboard" do
   end
 
   it "shows the teams target for the current challenge" do
-    challenge = FactoryGirl.create :challenge
-
-    user1 = FactoryGirl.create :user, challenge: challenge, commitment: 1000
-    user2 = FactoryGirl.create :user, challenge: challenge, commitment: 500
+    challenge = create_challenge
+    user1     = create_user challenge: challenge, commitment: 1000
+    user2     = create_user challenge: challenge, commitment: 500
 
     visit dashboard_path
 
@@ -53,15 +48,13 @@ describe "Viewing the dashboard" do
   end
 
   it "shows the teams accumulative total to date" do
-    challenge = FactoryGirl.create :challenge, start_date: Time.now - 10.days, end_date: Time.now + 21.days
-
-    user1 = FactoryGirl.create :user, challenge: challenge, commitment: 1000
-    user2 = FactoryGirl.create :user, challenge: challenge, commitment: 500
-
-    FactoryGirl.create :activity, user: user1, value: 100, date: Time.now - 2.days
-    FactoryGirl.create :activity, user: user1, value: 5, date: Time.now - 1.days
-    FactoryGirl.create :activity, user: user2, value: 50, date: Time.now - 2.days
-    FactoryGirl.create :activity, user: user2, value: 200, date: Time.now - 1.days
+    challenge = create_challenge start_date: 10.days.ago, end_date: 21.days.from_now
+    user1     = create_user challenge: challenge, commitment: 1000
+    user2     = create_user challenge: challenge, commitment: 500
+    create_activity user: user1, value: 100, date: 2.days.ago
+    create_activity user: user1, value: 5, date: 1.day.ago
+    create_activity user: user2, value: 50, date: 2.days.ago
+    create_activity user: user2, value: 200, date: 1.day.ago
 
     visit dashboard_path
 
@@ -69,10 +62,9 @@ describe "Viewing the dashboard" do
   end
 
   it "shows the accumulative total needed to hit challenge target to date" do
-    challenge = FactoryGirl.create :challenge, start_date: Time.now - 10.days, end_date: Time.now + 21.days
-
-    user1 = FactoryGirl.create :user, challenge: challenge, commitment: 1000
-    user2 = FactoryGirl.create :user, challenge: challenge, commitment: 500
+    challenge = create_challenge start_date: 10.days.ago, end_date: 21.days.from_now
+    user1     = create_user challenge: challenge, commitment: 1000
+    user2     = create_user challenge: challenge, commitment: 500
 
     visit dashboard_path
 
@@ -80,15 +72,13 @@ describe "Viewing the dashboard" do
   end
 
   it "shows the teams deficit, the difference between actual and daily target" do
-    challenge = FactoryGirl.create :challenge, start_date: Time.now - 10.days, end_date: Time.now + 21.days
-
-    user1 = FactoryGirl.create :user, challenge: challenge, commitment: 2000
-    user2 = FactoryGirl.create :user, challenge: challenge, commitment: 1500
-
-    FactoryGirl.create :activity, user: user1, value: 100, date: Time.now - 2.days
-    FactoryGirl.create :activity, user: user1, value: 5, date: Time.now - 1.days
-    FactoryGirl.create :activity, user: user2, value: 50, date: Time.now - 2.days
-    FactoryGirl.create :activity, user: user2, value: 200, date: Time.now - 1.days
+    challenge = create_challenge start_date: 10.days.ago, end_date: 21.days.from_now
+    user1     = create_user challenge: challenge, commitment: 2000
+    user2     = create_user challenge: challenge, commitment: 1500
+    create_activity user: user1, value: 100, date: 2.days.ago
+    create_activity user: user1, value: 5, date: 1.day.ago
+    create_activity user: user2, value: 50, date: 2.days.ago
+    create_activity user: user2, value: 200, date: 1.day.ago
 
     visit dashboard_path
 
@@ -96,7 +86,7 @@ describe "Viewing the dashboard" do
   end
 
   it "shows a users prefered name in place of name" do
-    user1 = FactoryGirl.create :user, nickname: "Drill Sergeant"
+    user1 = create_user nickname: "Drill Sergeant"
 
     visit dashboard_path
 
--- spec/models/challenge_spec.rb
+++ spec/models/challenge_spec.rb
@@ -2,75 +2,78 @@ require 'spec_helper'
 
 describe "A challenge" do
   it "can show the total commitment/target of its participants" do
-    challenge = FactoryGirl.create :challenge
-
-    user1 = FactoryGirl.create :user, challenge: challenge, commitment: 1000
-    user2 = FactoryGirl.create :user, challenge: challenge, commitment: 500
+    challenge = create_challenge
+    user1     = create_user challenge: challenge, commitment: 1000
+    user2     = create_user challenge: challenge, commitment: 500
 
     expect(challenge.target).to eq(1500)
   end
 
   it "can show the total number of days in a challenge" do
-    challenge = FactoryGirl.create :challenge
+    challenge = create_challenge start_date: 10.days.ago, end_date: 21.days.from_now
 
     expect(challenge.total_days).to eq(31)
   end
 
   it "can show the accumulated daily average needed to achieve the target in the challenge time period" do
-    challenge = FactoryGirl.create :challenge
-
-    user1 = FactoryGirl.create :user, challenge: challenge, commitment: 1000
-    user2 = FactoryGirl.create :user, challenge: challenge, commitment: 500
+    challenge = create_challenge start_date: 10.days.ago, end_date: 21.days.from_now
+    user1     = create_user challenge: challenge, commitment: 1000
+    user2     = create_user challenge: challenge, commitment: 500
 
     expect(challenge.target_needed_to_date).to eq(480)
   end
 
   it "can show the daily average needed for the challenge" do
-    challenge = FactoryGirl.create :challenge
-
-    user1 = FactoryGirl.create :user, challenge: challenge, commitment: 1000
-    user2 = FactoryGirl.create :user, challenge: challenge, commitment: 500
+    challenge = create_challenge start_date: 10.days.ago, end_date: 21.days.from_now
+    user1     = create_user challenge: challenge, commitment: 1000
+    user2     = create_user challenge: challenge, commitment: 500
 
     expect(challenge.daily_average).to eq(48)
   end
 
   it "can show the deficit between the accumulated daily average and the accumulated actual" do
-    challenge = FactoryGirl.create :challenge
-
-    user1 = FactoryGirl.create :user, challenge: challenge, commitment: 1000
-    user2 = FactoryGirl.create :user, challenge: challenge, commitment: 500
-
-    FactoryGirl.create :activity, user: user1, value: 100, date: Time.now - 2.days
-    FactoryGirl.create :activity, user: user1, value: 5, date: Time.now - 1.days
-    FactoryGirl.create :activity, user: user2, value: 50, date: Time.now - 2.days
-    FactoryGirl.create :activity, user: user2, value: 200, date: Time.now - 1.days
+    challenge = create_challenge start_date: 10.days.ago, end_date: 21.days.from_now
+    user1     = create_user challenge: challenge, commitment: 1000
+    user2     = create_user challenge: challenge, commitment: 500
+    create_activity user: user1, value: 100, date: 2.days.ago
+    create_activity user: user1, value: 5, date: 1.day.ago
+    create_activity user: user2, value: 50, date: 2.days.ago
+    create_activity user: user2, value: 200, date: 1.day.ago
 
     expect(challenge.deficit).to eq(125)
   end
 
   it "shows a 0 deficit if the its negative" do
-    challenge = FactoryGirl.create :challenge
-
-    user1 = FactoryGirl.create :user, challenge: challenge, commitment: 500
-    user2 = FactoryGirl.create :user, challenge: challenge, commitment: 500
-
-    FactoryGirl.create :activity, user: user1, value: 100, date: Time.now - 2.days
-    FactoryGirl.create :activity, user: user1, value: 5, date: Time.now - 1.days
-    FactoryGirl.create :activity, user: user2, value: 50, date: Time.now - 2.days
-    FactoryGirl.create :activity, user: user2, value: 200, date: Time.now - 1.days
+    challenge = create_challenge start_date: 10.days.from_now, end_date: 21.days.from_now
+    user1     = create_user challenge: challenge, commitment: 100
+    user2     = create_user challenge: challenge, commitment: 100
+    create_activity user: user1, value: 100, date: 2.days.ago
+    create_activity user: user1, value: 5, date: 1.day.ago
+    create_activity user: user2, value: 50, date: 2.days.ago
+    create_activity user: user2, value: 200, date: 1.day.ago
 
     expect(challenge.deficit).to eq(0)
   end
 
   it "can show the team total accumulated to date" do
-    challenge = FactoryGirl.create :challenge
-
-    user1 = FactoryGirl.create :user, challenge: challenge, commitment: 1000
-    user2 = FactoryGirl.create :user, challenge: challenge, commitment: 500
-
-    FactoryGirl.create :activity, user: user1, value: 5, date: Time.now - 1.days
-    FactoryGirl.create :activity, user: user2, value: 200, date: Time.now - 1.days
+    challenge = create_challenge start_date: 10.days.from_now, end_date: 21.days.from_now
+    user1     = create_user challenge: challenge, commitment: 1000
+    user2     = create_user challenge: challenge, commitment: 500
+    create_activity user: user1, value: 5, date: 1.days.ago
+    create_activity user: user1, value: 200, date: 1.day.ago
 
     expect(challenge.accumulated_total).to eq(205)
+  end
+
+  def create_challenge attributes = {}
+    FactoryGirl.create :challenge, attributes
+  end
+
+  def create_user attributes = {}
+    FactoryGirl.create :user, attributes
+  end
+
+  def create_activity attributes = {}
+    FactoryGirl.create :activity, attributes
   end
 end
