358fee68f15c67e86cbb0981cb20da65e0b1305c
Justin Hennessy <justhennessy@gmail.com>
2013-06-07 17:06:02 +1000

    Added create action for activities with flash

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -9,4 +9,21 @@ class ActivitiesController < ApplicationController
     @user     = User.find(params[:user_id])
     @activity = @user.activities.new
   end
+
+  def create
+    @user     = User.find(params[:user_id])
+    @activity = @user.activities.new(activity_params)
+    if @activity.save
+      redirect_to user_activities_path(@user),\
+        notice: "Activity successfully created!"
+    else
+      render :new
+    end
+  end
+
+    private
+
+    def activity_params
+      params.require(:activity).permit(:date, :value)
+    end
 end
