661138f1cb50b72d95bf3e9ad37fdde90572b0d2
Justin Hennessy <justhennessy@gmail.com>
2013-06-09 17:29:34 +1000

    Added pending tests for edit activities

---
+++ spec/features/edit_activity_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe "Editing an activity" do
+  it "updates the activity then shows the activity list"
+
+  it "doesn't update if the date of the activity is outside the challenge date"
+
+  it "doesn't update if the activity value is < zero"
+end
