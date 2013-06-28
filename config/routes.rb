ec5a4496429e55f53da7c6ecbd7a2e9a792233a5
Justin Hennessy <justhennessy@gmail.com>
2013-06-28 19:51:17 +1000

    Facebook omniauth setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 DmChallenge::Application.routes.draw do
   root "dashboard#index"
 
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destroy', as: 'signout'
+
   resources :challenges
-  
+
   resources :users do
     resources :activities
   end
