ebb9c6ecdfecd95d239f533f8154d701d5e95009
Justin Hennessy <justhennessy@gmail.com>
2013-07-20 21:06:27 +1000

    Revert "Added a raise as there is a omniauth callback issue"
    This reverts commit a46812a7e9baf3b07509eee2e8a1885a387432ca.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,6 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    raise
     redirect_to dashboard_url
   end
 
