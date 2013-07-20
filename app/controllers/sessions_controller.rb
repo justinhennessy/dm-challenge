a46812a7e9baf3b07509eee2e8a1885a387432ca
Justin Hennessy <justhennessy@gmail.com>
2013-07-20 20:52:46 +1000

    Added a raise as there is a omniauth callback issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
+    raise
     redirect_to dashboard_url
   end
 
