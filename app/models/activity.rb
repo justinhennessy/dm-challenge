416d451f4e973751afc88bb177fc0219c48ebb74
Justin Hennessy <justhennessy@gmail.com>
2013-08-08 20:30:19 +1000

    Added total_stat_between and between class methods

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,4 +4,11 @@ class Activity < ActiveRecord::Base
   validates :distance, presence: true, :numericality => { greater_than: 0 }
   validates :date, presence: true
 
+  def self.total_stat_between(period, stat_to_sum)
+    between(period).sum(stat_to_sum)
+  end
+
+  def self.between(period)
+    where("date >= ? and date <= ?", period.start, period.finish)
+  end
 end
