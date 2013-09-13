b133dc2e8958e7c7a70bb9431a100cc70fb9c733
Justin Hennessy <justhennessy@gmail.com>
2013-09-13 19:26:15 +1000

    Fixed some styling issues

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,8 +10,10 @@ describe "A user" do
   }
 
   before(:each) do
-    create_activity user: user, distance: 100, ascent: 20, achievements: 1, date: 2.days.ago
-    create_activity user: user, distance: 5, ascent: 10, achievements: 98, date: 1.day.ago
+    create_activity user: user, distance: 100, ascent: 20, achievements: 1,
+      date: 2.days.ago
+    create_activity user: user, distance: 5, ascent: 10, achievements: 98,
+      date: 1.day.ago
   end
 
   it "must not have a 0 commitment"
@@ -49,7 +51,7 @@ describe "A user" do
     expect(user.total_distance_for(challenge.period)).to eq(105)
   end
 
-    it "can show the % completed of a commitment" do
+  it "can show the % completed of a commitment" do
     expect(user.percent_completed).to eq(10)
   end
 
@@ -69,7 +71,8 @@ describe "A user" do
     before(:each) do
       @activity1 = create_activity user: user2, distance: 200, date: 1.day.ago
       @activity2 = create_activity user: user2, distance: 51, date: 5.days.ago
-      @activity3 = create_activity user: user2, distance: 50, date: 10.days.from_now
+      @activity3 = create_activity user: user2, distance: 50,
+        date: 10.days.from_now
       create_activity user: user2, distance: 50, date: 6.days.ago
       create_activity user: user2, distance: 50, date: 11.days.from_now
     end
@@ -79,7 +82,7 @@ describe "A user" do
     end
 
     it "can show actvities within a date range" do
-      expect(user2.activities_for(challenge.period)).to eq([@activity1,@activity2,@activity3])
+      expect(user2.activities_for(challenge.period)).to eq([@activity1, @activity2, @activity3])
     end
 
     it "can show if a user has the yellow jersey" do
